e5063b4e8e0ccf177fdc2c3eba3b3a1ed3aa0506
Mike0186 <>
2023-06-07 20:33:11 +0000

    Add start game function with user input validation

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
+
+def start_game():
+    """
+    Starts a new game. Asks the palyer to set the board size.
+    """
+    while True:
+        size = input("Please enter the size of you battlefield:")
+        if not size.isnumeric():
+            print("invalid Entry. Please input a positive integer.")
+            continue
+        size = int(size)
+        if size < 2 or size > 10:
+            print("Invalid Entry. Please choose a number between 2 and 10.")
+            continue
+        break
+    num_ships = min(size -1, 9)
+
+    print("-" * 120)
+    player_name = input("Please Enter Your Name:\n")
+    print("Welcome" + " " +(player_name.title()) + " " + "to ......\n")
+    print(" ********      **       ********    ********   **        ******  *****    **       **  ******    *****    ")
+    print(" **    **    **  **        **          **      **        **      **       **           **  **    **       ")
+    print(" **    **    **  **        **          **      **        **      **       **       **  **  **    **       ")
+    print(" ********    ******        **          **      **        *****   *****    ******   **  ******    ******   ")
+    print(" ********    ******        **          **      **        *****   *****    ******   **  ******    ******   ")
+    print(" **    **    **  **        **          **      **        **         **    **  **   **  **            **   ")
+    print(" **    **    **  **        **          **      **        **         **    **  **   **  **            **   ")
+    print(" ********    **  **        **          **      *****     ******  *****    **  **   **  **        ******   ")
+    print("-" * 120)
+
+    
+    print("-" * 50)
+
+start_game()    
+
+
+"""
 def user_grid_size():
     while True:
         try:
@@ -7,6 +44,18 @@ def user_grid_size():
             else:
                 return size
         except ValueError:
-            Print("Invalid input. Please enter a postive number")
+            print("Invalid input. Please enter a postive number")
+
+class board:
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guess = []
+        self.ships =[]
+print(board)
+
 grid_size = user_grid_size()            
-print(size)
+print(grid_size)
+"""

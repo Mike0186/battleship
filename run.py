84d7dea248a13ea3497becf61e075ab2db06e6db
Mike0186 <>
2023-06-09 22:58:42 +0000

    Add function to make a guess on game board

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 import random 
-from random import randint
 
 class Board:
     """
@@ -13,7 +12,7 @@ class Board:
         self.name = name
         self.type = board_type
         self.board = [['.' for _ in range(size)] for _ in range(size)]
-        self.guesses = []
+        self.guess_list = []
         self.ships = []
 
     def print_board(self):
@@ -29,13 +28,40 @@ class Board:
                     self.board[row][column] = "."
 
     # This code was cited in the code insitiute battleship game video
+
+    def guess(self, x, y):
+        self.guess_list.append((x,y))
+        self.board [x] [y] = "X"
+
+        if (x,y) in self.ships:
+            self.board [x] [y] = "*"
+            return "Hit"
+        else:
+            return "Miss"
+
     def add_ship(self, x, y, type ='computer'):
         if len(self.ships) >= self.num_ships:
             print("Error: you cannot add any more ships")
         else:
             self.ships.append((x, y))
-            if self.type == "player":
-                self.board[x] [y] = "@"            
+            if self.type =="player":
+                self.board[x] [y] = "@" 
+
+    def make_guess(self):
+        while True:
+            x = input("Select a row between 0" + " " + str(self.size) + "\n") 
+            y = input("Select a row between 0" + " " + str(self.size) + "\n")
+            if not x.isnumeric() or not y.isnumeric():
+                print("Invalid Entry! please enter a numeric postive integer between 0" + " "+ {self.size})
+                continue
+            x = int(x)
+            y = int(y)
+            result = self.guess(x,y)
+            if result == "Hit":
+                print("You hit a ship")
+            elif result == "Miss":  
+                print("You Missed")
+              
         
 
 def start_game():
@@ -45,7 +71,7 @@ def start_game():
     while True:
         size = input("Please enter the size of you battlefield:")
         if not size.isnumeric():
-            print("invalid Entry. Please input a positive integer.")
+            print("Invalid Entry. Please input a positive integer.")
             continue
         size = int(size)
         if size < 2 or size > 10:
@@ -69,15 +95,20 @@ def start_game():
 
     
     print("-" * 50)
+    
     player_board = Board(size, num_ships, 'player', board_type='player')
     for _ in range(num_ships):
         x, y = random.randint(0,player_board.size -1), random.randint(0,player_board.size -1)
         player_board.add_ship(x, y, type = 'player')
     player_board.populate_board()
-    computer_board = Board(size, num_ships, 'computer', board_type='computer')
-    x, y = random.randint(0,player_board.size -1), random.randint(0,player_board.size -1)
+    
+    
+    for _ in range(num_ships):
+        computer_board = Board(size, num_ships, 'computer', board_type='computer')
+        x, y = random.randint(0,player_board.size -1), random.randint(0,player_board.size -1)
     computer_board.add_ship(x, y, type = 'computer')
     computer_board.populate_board()
+    
     print("-" * 50)
     print((player_name.title()) + " " + "here is your battlefield. Your Ships are marked with the @ symbol \n")
     print("-" * 50)
@@ -86,9 +117,13 @@ def start_game():
     print("Computer here is your battlefield\n")
     computer_board.print_board()
 
+    player_board.make_guess()
+    
+
     
 
 start_game()
+
 
 
 
